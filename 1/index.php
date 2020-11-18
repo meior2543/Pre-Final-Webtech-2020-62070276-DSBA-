efc35b7ca59ba2cac6c83398cd123a67afad4b24
meior2543 <56358092+meior2543@users.noreply.github.com>
2020-11-18 21:33:05 +0700

    Add files via upload

---
+++ 1/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <?php
+            $url = "https://dd-wtlab2020.netlify.app/pre-final/ezquiz.json";
+            $response = file_get_contents($url);
+            $result = json_decode($response);
+
+
+            for ($num = 0; $num <= 19; $num++) {
+                echo "<div class='col-md-4 mb-3'>";
+                echo "<div class='card'>";
+                echo "<img class='card-img-top' src='" . $result->tracks->items[$num]->album->images[0]->url . "'>";
+                echo "<div class='card-body'>";
+                echo "<h5 class='card-title'>" . $result->tracks->items[$num]->album->name . "</h5>";
+                echo "<p class='card-text'>Artist:'" . $result->tracks->items[$num]->album->artists[0]->name . "'</p>";
+                echo "<p class='card-text'>Release date:'" . $result->tracks->items[$num]->album->release_date . "'</p>";
+                echo "<p class='card-text'>Available:'" . sizeof($result->tracks->items[$num]->available_markets) . "'</p>";
+                echo "</div>";
+                echo "</div>";
+                echo "</div>";
+            }
+            ?>
+        </div>
+    </div>
+</body>
+
+</html>
